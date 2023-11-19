cfb2cad56ced72424a660a22566b8bf9b96163cb
AhmedAbderrahmanElBaghdadi04 <baghdadiahmed004@gmail.com>
2023-11-19 19:25:01 +0100

    first-message

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <BrowserRouter basename='/Tp5_react'>
+  <BrowserRouter basename='/CC1_react'>
     <App />
   </BrowserRouter>
 );
